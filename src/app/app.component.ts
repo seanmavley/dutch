69ff879862b87d8a48a749f84cd7a040d236b045
KhoPhi <seanmavley@gmail.com>
2024-06-02 23:39:01 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dutch';
+  openStyle: 'side' | 'over' = 'over';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver) { }
+
+    ngOnInit(): void {  
+      this.breakpointObserver.observe([
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large
+      ]).subscribe(result => {
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.openStyle = 'side';
+        } else if (result.breakpoints[Breakpoints.Small] || result.breakpoints[Breakpoints.Medium]) {
+          this.openStyle = 'side';
+        }
+      });
+    }
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatRadioModule} from '@angular/material/radio';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatNavList,
+    MatListModule,
+    FormsModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatRadioModule
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatNavList,
+    FormsModule,
+    MatInputModule,
+    MatProgressBarModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatRadioModule
+  ]
+})
+export class SharedModule { }
