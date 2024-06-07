6902af62d2369680fef31d55bcb701252fb94e0b
KhoPhi <seanmavley@gmail.com>
2024-06-07 00:25:31 +0000

    initialized service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -150,3 +150,15 @@ export class AppComponent {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./app.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
